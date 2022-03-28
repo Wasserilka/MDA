77638f9638dc7b8fa94534cb0713bdc65e1f6a96
Vasiliy Pankrukhin <wacpan93@gmail.com>
2022-03-28 11:59:52 +0300

    решение к 1 и 2 заданию

--- MDA/Program.cs
+++ MDA/Program.cs
@@ -1,13 +1,14 @@
-﻿using MDA.Communication;
-using MDA.Models;
+﻿using MDA.Models;
 using System.Diagnostics;
 using System.Text;
+using System;
 
 Console.OutputEncoding = Encoding.UTF8;
-var communicationAgent = new CommunicationAgent();
-var rest = new Restaurant(communicationAgent);
+var rest = new Restaurant();
 while (true)
 {
+    await Task.Delay(3000);
+
     var strBuilder = new StringBuilder();
     strBuilder.Append("Привет! Желаете забронировать столик или снять бронь?");
     strBuilder.Append("\n1 - забронировать, мы уведомим вас по смс (асинхронно).");
@@ -17,35 +18,11 @@ while (true)
 
     Console.WriteLine(strBuilder.ToString());
 
-    int.TryParse(Console.ReadLine(), out var choice);
-    if (choice > 4 || choice < 1)
-    {
-         communicationAgent.SendMessage("Введите, пожалуйста, любой номер из указанных выше.");
-        continue;
-    }
-
-    switch (choice)
-    {
-        case 1:
-        case 2:
-             communicationAgent.SendMessage("Введите, пожалуйста, количество гостей.");
-            break;
-        case 3:
-        case 4:
-             communicationAgent.SendMessage("Введите, пожалуйста, номер столика.");
-            break;
-        default:
-            break;
-    }
-    int.TryParse(Console.ReadLine(), out var attribute);
-    if (attribute < 1)
-    {
-         communicationAgent.SendMessage("Введите, пожалуйста, положительное число.");
-        continue;
-    }
-
     var stopWatch = new Stopwatch();
     stopWatch.Start();
+
+    var choice = 1;
+    var attribute = 2;
     switch (choice)
     {
         case 1:
@@ -64,7 +41,7 @@ while (true)
             break;
     }
 
-    communicationAgent.SendMessage("Спасибо за обращение!");
+    Console.WriteLine("Спасибо за обращение!");
     stopWatch.Stop();
     var ts = stopWatch.Elapsed;
     Console.WriteLine($"{ts.Seconds:00}:{ts.Milliseconds:00}");
